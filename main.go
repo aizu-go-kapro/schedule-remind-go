0c6ca283d67bba24a56ad7d426ca252ea39edead
yt8492 <tmymyut2000@gmail.com>
2020-08-14 18:12:49 +0900

    serve bot server on main

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"github.com/line/line-bot-sdk-go/linebot"
+	"log"
+	"net/http"
+	"os"
+	"schedule-remind-go/service"
+)
+
 func main() {
+	bot, err := linebot.New(os.Getenv("LINE_CHANNEL_SECRET"), os.Getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	service.HandleLineEvent(bot, "/callback", func(client *linebot.Client, event *linebot.Event) {
+		// イベントごとの処理
+	})
+	http.ListenAndServe(":8080", nil)
 }
